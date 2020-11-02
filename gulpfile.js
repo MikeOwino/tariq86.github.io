83a0099f543fd26ab99b1ac2ad7229800eab915f
Tariq <tariq.abusheikh@gmail.com>
2020-11-01 22:42:00 -0600

    More cleanup/customization

--- gulpfile.js
+++ gulpfile.js
@@ -12,88 +12,88 @@ var cp = require('child_process');
  * Compile and minify sass
  */
 function styles() {
-  return gulp
-    .src([ '_sass/*.scss' ])
-    .pipe(
-      sass({
-        includePaths: [ 'scss' ],
-        onError: browserSync.notify
-      })
-    )
-    .pipe(prefix([ 'last 3 versions', '> 1%', 'ie 8' ], { cascade: true }))
-    .pipe(rename('main.min.css'))
-    .pipe(cssnano())
-    .pipe(gulp.dest('_site/assets/css/'))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest('assets/css'));
+    return gulp
+        .src(['_sass/*.scss'])
+        .pipe(
+            sass({
+                includePaths: ['scss'],
+                onError: browserSync.notify
+            })
+        )
+        .pipe(prefix(['last 3 versions', '> 1%', 'ie 8'], { cascade: true }))
+        .pipe(rename('main.min.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('_site/assets/css/'))
+        .pipe(browserSync.reload({ stream: true }))
+        .pipe(gulp.dest('assets/css'));
 }
 
 function stylesVendors() {
-  return gulp
-    .src([ '_sass/vendors/*.css' ])
-    .pipe(concat('vendors.min.css'))
-    .pipe(cssnano())
-    .pipe(gulp.dest('_site/assets/css/'))
-    .pipe(gulp.dest('assets/css'));
+    return gulp
+        .src(['_sass/vendors/*.css'])
+        .pipe(concat('vendors.min.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('_site/assets/css/'))
+        .pipe(gulp.dest('assets/css'));
 }
 
 /**
  * Compile and minify js
  */
 function scripts() {
-  return gulp
-    .src([ '_js/app.js' ])
-    .pipe(rename('app.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('_site/assets/js'))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest('assets/js'));
+    return gulp
+        .src(['_js/app.js'])
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('_site/assets/js'))
+        .pipe(browserSync.reload({ stream: true }))
+        .pipe(gulp.dest('assets/js'));
 }
 
 function scriptsVendors() {
-  return gulp
-    .src([ '_js/vendors/*.js' ])
-    .pipe(concat('vendors.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('_site/assets/js'))
-    .pipe(gulp.dest('assets/js'));
+    return gulp
+        .src(['_js/vendors/*.js'])
+        .pipe(concat('vendors.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('_site/assets/js'))
+        .pipe(gulp.dest('assets/js'));
 }
 
 /**
  * Server functionality handled by BrowserSync
  */
 function browserSyncServe(done) {
-  browserSync.init({
-    server: '_site',
-    port: 4000
-  });
-  done();
+    browserSync.init({
+        server: '_site',
+        port: 4000
+    });
+    done();
 }
 
 function browserSyncReload(done) {
-  browserSync.reload();
-  done();
+    browserSync.reload();
+    done();
 }
 
 /**
  * Build Jekyll site
  */
 function jekyll(done) {
-  return cp
-    .spawn(
-      'bundle',
-      [
-        'exec',
-        'jekyll',
-        'build',
-        '--incremental',
-        '--config=_config.yml,_config_dev.yml'
-      ],
-      {
-        stdio: 'inherit'
-      }
-    )
-    .on('close', done);
+    return cp
+        .spawn(
+            'bundle',
+            [
+                'exec',
+                'jekyll',
+                'build',
+                '--incremental',
+                '--config=_config.yml,_config_dev.yml'
+            ],
+            {
+                stdio: 'inherit'
+            }
+        )
+        .on('close', done);
 }
 
 /**
@@ -101,29 +101,25 @@ function jekyll(done) {
  * Watch html/md files, run Jekyll & reload BrowserSync
  */
 function watchData() {
-  gulp.watch(
-    [ '_data/*.yml', '_config.yml', 'assets/*.json' ],
-    gulp.series(jekyll, browserSyncReload)
-  );
+    gulp.watch(
+        ['_data/*.yml', '_config.yml', 'assets/*.json'],
+        gulp.series(jekyll, browserSyncReload)
+    );
 }
 
 function watchMarkup() {
-  gulp.watch(
-    [ 'index.html', '_includes/*.html', '_layouts/*.html' ],
-    gulp.series(jekyll, browserSyncReload)
-  );
+    gulp.watch(
+        ['index.html', '_includes/*.html', '_layouts/*.html'],
+        gulp.series(jekyll, browserSyncReload)
+    );
 }
 
 function watchScripts() {
-  gulp.watch([ '_js/*.js' ], scripts);
+    gulp.watch(['_js/*.js'], scripts);
 }
 
 function watchStyles() {
-  gulp.watch([ '_sass/*.scss' ], styles);
-}
-
-function watch() {
-  gulp.parallel(watchData, watchMarkup, watchScripts, watchStyles);
+    gulp.watch(['_sass/*.scss'], styles);
 }
 
 var compile = gulp.parallel(styles, stylesVendors, scripts, scriptsVendors);
